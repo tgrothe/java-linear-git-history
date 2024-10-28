ebd714d858dd04a6318184de89e6b2a5ae9a981b
Tobias Grothe <mail@tobias-1.de>
2024-10-28 18:23:55 +0100

    Update fix 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Main {
   public static void main(String[] args) throws IOException, InterruptedException {
-    System.out.print("Enter path to repo: ");
+    System.out.print("Enter full path to repo: ");
     String path = new Scanner(System.in, Charset.defaultCharset()).nextLine();
     System.out.print("Enter main branch name: ");
     String main = new Scanner(System.in, Charset.defaultCharset()).nextLine();
@@ -14,8 +14,8 @@ public class Main {
   }
 
   private static void linearize1(File repo, String main) throws IOException, InterruptedException {
-    List<String> branches = exec(repo, "git", "branch", "-l");
-    branches.remove(0);
+    List<String> branches = exec(repo, "git", "branch");
+    branches.remove(0); // status int
     branches =
         branches.stream()
             .map(
@@ -30,14 +30,17 @@ public class Main {
     //    branches.sort(Comparator.reverseOrder());
     //    Collections.shuffle(branches);
     HashMap<Map.Entry<String, String>, Boolean> ancestorMap = new HashMap<>();
-    for (String b1 : branches) {
-      for (String b2 : branches) {
-        if (!b1.equals(b2)) {
+    for (int i = 0; i < branches.size(); i++) {
+      String b1 = branches.get(i);
+      for (int j = 0; j < branches.size(); j++) {
+        if (i != j) {
+          String b2 = branches.get(j);
           boolean b = "0".equals(exec(repo, "git", "merge-base", "--is-ancestor", b1, b2).get(0));
           ancestorMap.put(new AbstractMap.SimpleEntry<>(b1, b2), b);
         }
       }
     }
+    System.out.println("ancestorMap = " + ancestorMap);
     TreeMap<String, List<String>> descendants = new TreeMap<>();
     for (int i = 0; i < branches.size(); i++) {
       String b1 = branches.get(i);
@@ -46,33 +49,32 @@ public class Main {
           continue;
         }
         String b2 = branches.get(j);
-        if (ancestorMap.get(new AbstractMap.SimpleEntry<>(b2, b1))) {
-          System.out.printf("%s is ancestor of %s.%n", b2, b1);
+        if (ancestorMap.get(new AbstractMap.SimpleEntry<>(b1, b2))) {
+          System.out.printf("%s is ancestor of %s.%n", b1, b2);
           boolean isDirectAncestor = true;
           for (int k = 0; k < branches.size(); k++) {
             if (k == i || k == j) {
               continue;
             }
             String b3 = branches.get(k);
-            if (!ancestorMap.get(new AbstractMap.SimpleEntry<>(b3, b2))
-                && ancestorMap.get(new AbstractMap.SimpleEntry<>(b3, b1))) {
+            if (ancestorMap.get(new AbstractMap.SimpleEntry<>(b3, b2))
+                && !ancestorMap.get(new AbstractMap.SimpleEntry<>(b3, b1))) {
               isDirectAncestor = false;
               break;
             }
           }
           if (isDirectAncestor) {
-            System.out.printf("%s is direct ancestor of %s.%n", b2, b1);
-            if (!descendants.containsKey(b2)) {
-              descendants.put(b2, new ArrayList<>());
+            System.out.printf("%s is direct ancestor of %s.%n", b1, b2);
+            if (!descendants.containsKey(b1)) {
+              descendants.put(b1, new ArrayList<>());
             }
-            descendants.get(b2).add(b1);
-            //            break;
+            descendants.get(b1).add(b2);
           }
         }
       }
     }
     System.out.println("descendants = " + descendants);
-    linearize2(repo, main, descendants);
+    // linearize2(repo, main, descendants);
   }
 
   private static boolean areEqual(List<String> a, List<String> b) {
@@ -130,26 +132,3 @@ public class Main {
     return lines;
   }
 }
-
-//    HashMap<String, List<String>> revs = new HashMap<>();
-//    for (String b1 : branches) {
-//      revs.put(b1, exec(repo, "git", "rev-parse", b1));
-//    }
-//    HashMap<Map.Entry<String, String>, List<String>> bases = new HashMap<>();
-//    for (String b1 : branches) {
-//      for (String b2 : branches) {
-//        AbstractMap.SimpleEntry<String, String> k = new AbstractMap.SimpleEntry<>(b1, b2);
-//        if (!b1.equals(b2) && !bases.containsKey(k)) {
-//          bases.put(k, exec(repo, "git", "merge-base", b1, b2));
-//        }
-//      }
-//    }
-//    HashMap<Map.Entry<String, String>, Integer> merges = new HashMap<>();
-//    for (String b1 : branches) {
-//      for (String b2 : branches) {
-//        int m =
-//            Integer.parseInt(
-//                exec(repo, "git", "rev-list", "--count", "--merges", b1 + ".." + b2).get(1));
-//        merges.put(new AbstractMap.SimpleEntry<>(b1, b2), m);
-//      }
-//    }
